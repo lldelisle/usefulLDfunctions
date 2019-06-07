a863ade04b34d729d78f5d599a43e08d3eda6928
Lucille Delisle <lucille.delisle@epfl.ch>
2019-06-07 09:40:06 +0200

    changed grFromBedFileWithName to grFromBedFile because it was too restrictive

--- R/myGRBasicFunctions.R
+++ R/myGRBasicFunctions.R
@@ -12,11 +12,11 @@
 #' #   seqnames start end score
 #' # 1     chr1     0  10    20
 #' # 2     chr1    10  12    30
-bedGraphFromGR<-function(gr){
+bedGraphFromGR <- function(gr){
   # require package base
-  bdg<-data.frame(gr)
-  bdg$start<-bdg$start-1
-  return(bdg[,c("seqnames","start","end","score")])
+  bdg <- data.frame(gr)
+  bdg$start <- bdg$start - 1
+  return(bdg[, c("seqnames", "start", "end", "score")])
 }
 
 #' create a GenomicRanges object from a narrowPeak with only one entry per peak (even if multiple summits) sorted by score
@@ -25,22 +25,29 @@ bedGraphFromGR<-function(gr){
 #' @return a Genomic ranges sorted by score decreasing with one line per peak, the summit kept is the one with the higher score
 #' @importFrom GenomicRanges makeGRangesFromDataFrame
 #' @export
-grSortedSimplifiedFromNarrowPeak<-function(fn){
+grSortedSimplifiedFromNarrowPeak <- function(fn){
   # require package GenomicRanges
   # require function .readFileFromConditionOnNcols
   # I read the file from the first line with 10 columns (I remove the header)
   df <- .readFileFromConditionOnNcols(fn, "== 10")
-  colnames(df)<-c("chr","start","end","name","score","strand","signalValue","pValue","qValue","relativeSummit")
-  # I want to have only one entry per peak by default you may have multiple summits for one peak
-  dfA<-stats::aggregate(x=list(score=df$score),by=list(chr=df$chr,start=df$start,end=df$end),FUN=max)
-  dfA<-dfA[order(dfA$score,decreasing = T),]
-  myWishedRowNames<-paste0(df$chr,":",df$start,"-",df$end,"_",df$score)
+  colnames(df) <- c("chr", "start", "end", "name",
+                    "score", "strand", "signalValue",
+                    "pValue", "qValue", "relativeSummit")
+  # I want to have only one entry per peak by default
+  # you may have multiple summits for one peak
+  dfA <- stats::aggregate(x = list(score = df$score),
+                          by = list(chr = df$chr,
+                                    start = df$start, end = df$end),
+                          FUN = max)
+  dfA <- dfA[order(dfA$score, decreasing = T), ]
+  myWishedRowNames <- paste0(df$chr, ":", df$start, "-", df$end, "_", df$score)
   # You may have multiple summits in the same peak which have the same score
-  rownames(df)<-make.unique(myWishedRowNames)
-  rownames(dfA)<-paste0(dfA$chr,":",dfA$start,"-",dfA$end,"_",dfA$score)
+  rownames(df) <- make.unique(myWishedRowNames)
+  rownames(dfA) <- paste0(dfA$chr, ":", dfA$start, "-", dfA$end, "_", dfA$score)
   # Arbitrary the first one will be used
-  dfAwithMoreCols<-df[rownames(dfA),]
-  myAwithMoreColsGR<-makeGRangesFromDataFrame(dfAwithMoreCols,keep.extra.columns = T)
+  dfAwithMoreCols <- df[rownames(dfA), ]
+  myAwithMoreColsGR <- makeGRangesFromDataFrame(dfAwithMoreCols,
+                                                keep.extra.columns = T)
   return(myAwithMoreColsGR)
 }
 
@@ -50,12 +57,16 @@ grSortedSimplifiedFromNarrowPeak<-function(fn){
 #' @return a Genomic ranges with intervals (1-based closed)
 #' @importFrom GenomicRanges makeGRangesFromDataFrame
 #' @export
-grFromBedFileWithName<-function(fn){
+#' @examples
+#' tests_dir <- system.file("tests", package="usefulLDfunctions")
+#' test_bed <- file.path(tests_dir, "test3colWithoutHeader.bed")
+#'
+#' # Load the bed with no header in a dataframe
+#' grBED <- grFromBedFileWithName(test_bed)
+grFromBedFile <- function(fn){
   # require package GenomicRanges
   # require function readBed
-  temp.df<-readBed(fn)
-  colnames(temp.df)<-c("seqname","start","end","name")
-  temp.df$start<-temp.df$start+1
-  return(makeGRangesFromDataFrame(temp.df,keep.extra.columns = T))
+  temp.df <- readBed(fn)
+  temp.df$start <- temp.df$start + 1
+  return(makeGRangesFromDataFrame(temp.df, keep.extra.columns = T))
 }
-
