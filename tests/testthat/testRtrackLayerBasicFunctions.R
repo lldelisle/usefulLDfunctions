9097e52ca8093687a3b507fb916df7f7a377490f
Lucille Delisle <lucille.delisle@epfl.ch>
2019-06-24 10:14:04 +0200

    removed all functions which depend on external packages

--- tests/testthat/testRtrackLayerBasicFunctions.R
+++
@@ -1,9 +0,0 @@
-context("Rtracklayer Basic functions")
-library(usefulLDfunctions)
-test_that("getTSSinUCSCFormatFromEnsemblGTF gives what is expected", {
-  tests_dir <- system.file("tests", package = "usefulLDfunctions")
-  test_gtf <- file.path(tests_dir,
-                        "Homo_sapiens.GRCh38.95_491firstLines.gtf.gz")
-  load(file.path(tests_dir, "tss.Rdata"))
-  expect_equal(getTSSinUCSCFormatFromEnsemblGTF(test_gtf), expectedResult)
-})
